4bcf40946223a0d65ba11b628582063ce596b987
Pedro <pedro.hm.santos@hotmail.com>
2026-07-04 10:54:17 -0300

    Versão 3.5 com busca e verificação de alunos, sistema de verificação de aprovação e verificação de notas válidas

--- Sistema_Notas3.0.py
+++ Sistema_Notas3.0.py
@@ -3,11 +3,11 @@ nomes = []
 notas = []
 #Loop Meste
 while True:
-    resposta = input("Deseja cadastrar um aluno? S/N? ").upper()
-    if resposta == "S":
+    cadastrar = input("Deseja cadastrar um aluno? S/N? ").upper()
+    if cadastrar == "S":
 #Loop de Cadastro de nomes
         while True:
-            nome = input("Qual o nome do aluno? ")
+            nome = input("Qual o nome do aluno? ").upper()
             if nome == "":
                 print("Entrada inválida, tente novamente")
             else:
@@ -23,8 +23,35 @@ while True:
                 print("Nota Cadastrada com sucesso")
                 notas.append (nota)
                 break
-    elif resposta =="N":
+    elif cadastrar =="N":
         print("Fim do Cadastro")
         break
     else:
         print("Entrada inválida, tente novamente")
+while True:
+    resultado = input("Deseja verificar o resultado do aluno? S/N?").upper()
+    if resultado == "S":
+        while True:
+            nome = input("Qual o nome do aluno? ").upper()
+            if nome in nomes:
+                print("Aluno encontrado")
+                index = nomes.index(nome)
+                nota = notas[index]
+                if nota == 10:
+                    print("Excelente!")
+                    print(nome,"você foi aprovado com excelência, parabéns!")
+                elif nota >= 7:
+                    print("Aprovado")
+                    print(nome,"você foi aprovado!")
+                elif nota >= 5:
+                    print("Recuperação")
+                    print(nome,"precisa estudar mais na recuperação")
+                elif nota >= 0:
+                    print("Reprovado")
+                    print(nome,"você foi reprovado")
+                break
+            else:
+                print("Aluno não encontrado, tente novamente")
+    elif resultado =="N":
+        print("Programa encerrado")
+        break
